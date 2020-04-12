16ae109d3af8fc5e2f63d9315d0258f070180a3e
Dmitry Maevsky <dmitry.maevsky@gmail.com>
2020-04-12 19:24:48 +0900

    Switch testing to Ava

--- src/grammar.spec.js
+++ src/grammar.spec.js
@@ -1,40 +1,39 @@
+const test = require('ava');
+
 const Parser = require('rd-parse');
 const Grammar = require('./grammar');
 
-describe('Testing jsexpr grammar', () => {
 
-  const parser = Parser(Grammar);
+const parser = Parser(Grammar);
 
-  it('tests Identifier', () => {
-    let ast = parser(' bla ');
-    expect(ast).toEqual({ type: 'Identifier', name: 'bla', pos: 1 });
-  });
+test('Identifier', t => {
+  let ast = parser(' bla ');
+  t.deepEqual(ast, { type: 'Identifier', name: 'bla', pos: 1 });
+});
 
-  it('tests a string literal', () => {
-    let ast = parser(' "bla \\" bla" ');
-    expect(ast).toEqual({ type: 'Literal', value: 'bla " bla', raw: '"bla \\" bla"' });
-  });
+test('a string literal', t => {
+  let ast = parser(' "bla \\" bla" ');
+  t.deepEqual(ast, { type: 'Literal', value: 'bla " bla', raw: '"bla \\" bla"' });
+});
 
-  it('tests a number literal', () => {
-    let ast = parser(' 5e2 ');
-    expect(ast).toEqual({ type: 'Literal', value: 500, raw: '5e2' });
-  });
+test('a number literal', t => {
+  let ast = parser(' 5e2 ');
+  t.deepEqual(ast, { type: 'Literal', value: 500, raw: '5e2' });
+});
 
-  it('tests ArrowFunction, simple', () => {
-    let ast = parser('x => x * x');
-    expect(ast.type).toBe('ArrowFunction');
-    expect(ast.parameters.bound.length).toBe(1);
-    expect(ast.parameters.bound[0]).toEqual({ type: 'Identifier', name: 'x', pos: 0 });
-    expect(ast.parameters.rest).toBeUndefined();
-    expect(ast.result.type).toBe('BinaryExpression');
-  });
+test('ArrowFunction, simple', t => {
+  let ast = parser('x => x * x');
+  t.is(ast.type, 'ArrowFunction');
+  t.is(ast.parameters.bound.length, 1);
+  t.deepEqual(ast.parameters.bound[0], { type: 'Identifier', name: 'x', pos: 0 });
+  t.is(ast.parameters.rest, undefined);
+  t.is(ast.result.type, 'BinaryExpression');
+});
 
-  it('tests ArrowFunction, with initializer and rest parameter', () => {
-    let ast = parser('(c = 1, ...a) => c + a.length');
-    expect(ast.type).toBe('ArrowFunction');
-    expect(ast.parameters.bound.length).toBe(1);
-    expect(ast.parameters.bound[0].initializer).toEqual({ type: 'Literal', value: 1, raw: '1' });
-    expect(ast.parameters.rest).toEqual({ type: 'Identifier', name: 'a', pos: 11 });
-  });
-
+test('ArrowFunction, with initializer and rest parameter', t => {
+  let ast = parser('(c = 1, ...a) => c + a.length');
+  t.is(ast.type, 'ArrowFunction');
+  t.is(ast.parameters.bound.length, 1);
+  t.deepEqual(ast.parameters.bound[0].initializer, { type: 'Literal', value: 1, raw: '1' });
+  t.deepEqual(ast.parameters.rest, { type: 'Identifier', name: 'a', pos: 11 });
 });
