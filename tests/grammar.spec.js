274e130a0e2f027086d6d3e5419cdd66b4a2fe43
Dmitry Maevsky <dmitry.maevsky@gmail.com>
2022-11-12 21:07:53 +0900

    ava -> node:test + usnap + utap

--- tests/grammar.spec.js
+++ tests/grammar.spec.js
@@ -1,215 +1,220 @@
-import test from 'ava';
+import test from 'node:test';
+import assert from 'node:assert/strict';
+
+import snapshot from 'usnap';
+
+snapshot.setup(import.meta.url);
 
 import Parser from 'rd-parse';
 import Grammar from '../src/grammar.js';
 
 const parser = Parser(Grammar);
 
-test('empty input', t => {
-  t.throws(() => parser(''), { instanceOf: Error, message: 'Unexpected token at 1:1. Remainder: ' });
+test('empty input', () => {
+  assert.throws(() => parser(''), new Error('Unexpected token at 1:1. Remainder: '));
 });
 
-test('faulty input', t => {
-  t.throws(() => parser('a + \n]'), { instanceOf: Error, message: 'Unexpected token at 2:1. Remainder: ]' });
+test('faulty input', () => {
+  assert.throws(() => parser('a + \n]'), new Error('Unexpected token at 2:1. Remainder: ]'));
 });
 
-test('Identifier', t => {
+test('Identifier', () => {
   const ast = parser(' bla ');
-  t.deepEqual(ast, { type: 'Identifier', name: 'bla' });
-  t.is(ast.pos, 1);
+  assert.deepEqual(ast, { type: 'Identifier', name: 'bla' });
+  assert.equal(ast.pos, 1);
 });
 
 test('boolean literals', t => {
   const ast = parser('false || true');
-  t.snapshot(ast);
+  snapshot(ast, t.name);
 });
 
-test('a string literal', t => {
+test('a string literal', () => {
   const ast = parser(' "bla \\" bla" ');
-  t.deepEqual(ast, { type: 'Literal', value: 'bla " bla', raw: '"bla \\" bla"' });
+  assert.deepEqual(ast, { type: 'Literal', value: 'bla " bla', raw: '"bla \\" bla"' });
 });
 
-test('a number literal', t => {
+test('a number literal', () => {
   const ast = parser(' 5e2 ');
-  t.deepEqual(ast, { type: 'Literal', value: 500, raw: '5e2' });
+  assert.deepEqual(ast, { type: 'Literal', value: 500, raw: '5e2' });
 });
 
 test('operator precedence', t => {
   const ast = parser('x*x + y*y');
-  t.snapshot(ast);
+  snapshot(ast, t.name);
 });
 
 test('left associativity', t => {
   const ast = parser('a + b - c');
-  t.snapshot(ast);
+  snapshot(ast, t.name);
 });
 
 test('exponentiation operator (right associativity)', t => {
   const ast = parser('a ** b ** c / 2');
-  t.snapshot(ast);
+  snapshot(ast, t.name);
 });
 
-test('ArrowFunction, simple', t => {
+test('ArrowFunction, simple', () => {
   const ast = parser('x => x * x');
-  t.is(ast.type, 'ArrowFunction');
-  t.deepEqual(ast.parameters, { bindingType: 'SingleName', name: 'x' });
-  t.is(ast.parameters.rest, undefined);
-  t.is(ast.result.type, 'BinaryExpression');
+  assert.equal(ast.type, 'ArrowFunction');
+  assert.deepEqual(ast.parameters, { bindingType: 'SingleName', name: 'x' });
+  assert.equal(ast.parameters.rest, undefined);
+  assert.equal(ast.result.type, 'BinaryExpression');
 });
 
-test('ArrowFunction, with initializer and rest parameter', t => {
+test('ArrowFunction, with initializer and rest parameter', () => {
   const ast = parser('(c = 1, ...a) => c + a.length');
-  t.is(ast.type, 'ArrowFunction');
-  t.is(ast.parameters.bound.length, 1);
-  t.is(ast.parameters.bindingType, 'FormalParameters');
-  t.deepEqual(ast.parameters.bound[0].pattern, { bindingType: 'SingleName', name: 'c' });
-  t.deepEqual(ast.parameters.bound[0].initializer, { type: 'Literal', value: 1, raw: '1' });
-  t.deepEqual(ast.parameters.rest, { bindingType: 'SingleName', name: 'a' });
+  assert.equal(ast.type, 'ArrowFunction');
+  assert.equal(ast.parameters.bound.length, 1);
+  assert.equal(ast.parameters.bindingType, 'FormalParameters');
+  assert.deepEqual(ast.parameters.bound[0].pattern, { bindingType: 'SingleName', name: 'c' });
+  assert.deepEqual(ast.parameters.bound[0].initializer, { type: 'Literal', value: 1, raw: '1' });
+  assert.deepEqual(ast.parameters.rest, { bindingType: 'SingleName', name: 'a' });
 });
 
-test('template literals', t => {
+test('template literals', () => {
   const ast = parser('`${a} + ${b} is ${a + b}`');
-  t.is(ast.type, 'TemplateLiteral');
-  t.deepEqual(ast.parts[0], ['expressions', { type: 'Identifier', name: 'a' }]);
-  t.deepEqual(ast.parts[1], ['chunks', ' + ']);
-  t.deepEqual(ast.parts[2], ['expressions', { type: 'Identifier', name: 'b' }]);
-  t.deepEqual(ast.parts[3], ['chunks', ' is ']);
-  t.is(ast.parts[4][1].type, 'BinaryExpression');
+  assert.equal(ast.type, 'TemplateLiteral');
+  assert.deepEqual(ast.parts[0], ['expressions', { type: 'Identifier', name: 'a' }]);
+  assert.deepEqual(ast.parts[1], ['chunks', ' + ']);
+  assert.deepEqual(ast.parts[2], ['expressions', { type: 'Identifier', name: 'b' }]);
+  assert.deepEqual(ast.parts[3], ['chunks', ' is ']);
+  assert.equal(ast.parts[4][1].type, 'BinaryExpression');
 });
 
 test('template litarals 2', t => {
   const input = '`Mismatched timing labels (expected ${this.current_timing.label}, got ${label})`';
-  t.snapshot(parser(input));
+  snapshot(parser(input), t.name);
 });
 
 test('object literal short notation', t => {
   const input = '{ foo }';
   const ast = parser(input);
-  t.snapshot(ast);
+  snapshot(ast, t.name);
 
-  t.is(ast.properties[0].name, 'foo');
-  t.is(ast.properties[0].value.pos, 2);
-  t.is(ast.properties[0].value.text, 'foo');
+  assert.equal(ast.properties[0].name, 'foo');
+  assert.equal(ast.properties[0].value.pos, 2);
+  assert.equal(ast.properties[0].value.text, 'foo');
 });
 
-test('pos and text for member and call expressions', t => {
+test('pos and text for member and call expressions', () => {
   const input = 'obj.method(a, b)';
   const ast = parser(' ' + input + ' ');
 
-  t.is(ast.type, 'CallExpression');
-  t.is(ast.pos, 1);
-  t.is(ast.text, input);
-  t.is(ast.callee.type, 'MemberExpression');
-  t.is(ast.callee.pos, 1);
-  t.is(ast.callee.text, 'obj.method');
-  t.is(ast.callee.object.type, 'Identifier');
-  t.is(ast.callee.object.pos, 1);
-  t.is(ast.callee.object.text, 'obj');
+  assert.equal(ast.type, 'CallExpression');
+  assert.equal(ast.pos, 1);
+  assert.equal(ast.text, input);
+  assert.equal(ast.callee.type, 'MemberExpression');
+  assert.equal(ast.callee.pos, 1);
+  assert.equal(ast.callee.text, 'obj.method');
+  assert.equal(ast.callee.object.type, 'Identifier');
+  assert.equal(ast.callee.object.pos, 1);
+  assert.equal(ast.callee.object.text, 'obj');
 });
 
-test('pos and text for unary expressions', t => {
+test('pos and text for unary expressions', () => {
   const input = 'typeof ~foo';
   const ast = parser(input);
 
-  t.is(ast.type, 'UnaryExpression');
-  t.is(ast.operator, 'typeof');
-  t.is(ast.argument.type, 'UnaryExpression');
-  t.is(ast.argument.operator, '~');
-  t.is(ast.argument.pos, 7);
-  t.is(ast.argument.text, '~foo');
+  assert.equal(ast.type, 'UnaryExpression');
+  assert.equal(ast.operator, 'typeof');
+  assert.equal(ast.argument.type, 'UnaryExpression');
+  assert.equal(ast.argument.operator, '~');
+  assert.equal(ast.argument.pos, 7);
+  assert.equal(ast.argument.text, '~foo');
 });
 
-test('pos and text for binary expressions', t => {
+test('pos and text for binary expressions', () => {
   const input = 'a + 2 * b';
   const ast = parser(input);
 
-  t.is(ast.type, 'BinaryExpression');
-  t.is(ast.operator, '+');
-  t.is(ast.text, input);
-  t.is(ast.right.type, 'BinaryExpression');
-  t.is(ast.right.operator, '*');
-  t.is(ast.right.pos, 4);
-  t.is(ast.right.text, '2 * b');
+  assert.equal(ast.type, 'BinaryExpression');
+  assert.equal(ast.operator, '+');
+  assert.equal(ast.text, input);
+  assert.equal(ast.right.type, 'BinaryExpression');
+  assert.equal(ast.right.operator, '*');
+  assert.equal(ast.right.pos, 4);
+  assert.equal(ast.right.text, '2 * b');
 });
 
-test('pos and text for bound names', t => {
+test('pos and text for bound names', () => {
   const input = '(a, ...r) => a + r[0]';
   const ast = parser(input);
 
-  t.is(ast.type, 'ArrowFunction');
-  t.is(ast.parameters.bound[0].pattern.bindingType, 'SingleName');
-  t.is(ast.parameters.bound[0].pattern.pos, 1);
-  t.is(ast.parameters.rest.bindingType, 'SingleName');
-  t.is(ast.parameters.rest.pos, 7);
+  assert.equal(ast.type, 'ArrowFunction');
+  assert.equal(ast.parameters.bound[0].pattern.bindingType, 'SingleName');
+  assert.equal(ast.parameters.bound[0].pattern.pos, 1);
+  assert.equal(ast.parameters.rest.bindingType, 'SingleName');
+  assert.equal(ast.parameters.rest.pos, 7);
 });
 
-test('new expression + memeber expression', t => {
+test('new expression + memeber expression', () => {
   const input = 'new Array(3).length';
   const ast = parser(input);
 
-  t.is(ast.type, 'MemberExpression');
-  t.is(ast.object.type, 'NewExpression');
-  t.is(ast.object.ctor.type, 'Identifier');
-  t.is(ast.object.ctor.pos, 4);
+  assert.equal(ast.type, 'MemberExpression');
+  assert.equal(ast.object.type, 'NewExpression');
+  assert.equal(ast.object.ctor.type, 'Identifier');
+  assert.equal(ast.object.ctor.pos, 4);
 });
 
-test('new expression + call expression', t => {
+test('new expression + call expression', () => {
   const input = 'new Array(3).map((_, i) => i)';
   const ast = parser(input);
 
-  t.is(ast.type, 'CallExpression');
-  t.is(ast.callee.type, 'MemberExpression');
-  t.is(ast.callee.object.type, 'NewExpression');
-  t.is(ast.callee.object.ctor.type, 'Identifier');
-  t.is(ast.callee.object.ctor.pos, 4);
+  assert.equal(ast.type, 'CallExpression');
+  assert.equal(ast.callee.type, 'MemberExpression');
+  assert.equal(ast.callee.object.type, 'NewExpression');
+  assert.equal(ast.callee.object.ctor.type, 'Identifier');
+  assert.equal(ast.callee.object.ctor.pos, 4);
 });
 
-test('pos and text for arrow functions', t => {
+test('pos and text for arrow functions', () => {
   const input = ' () => x ';
   const ast = parser(input);
 
-  t.is(ast.type, 'ArrowFunction');
-  t.is(ast.pos, 1);
-  t.is(ast.text, '() => x');
+  assert.equal(ast.type, 'ArrowFunction');
+  assert.equal(ast.pos, 1);
+  assert.equal(ast.text, '() => x');
 });
 
 test('object literal with spread', t => {
   const input = '{ foo: 5, ...bar }';
   const ast = parser(input);
 
-  t.snapshot(ast);
+  snapshot(ast, t.name);
 
-  t.is(ast.type, 'ObjectLiteral');
-  t.is(ast.properties[0].name, 'foo');
-  t.is(ast.properties[1].spread.type, 'Identifier');
-  t.is(ast.properties[1].spread.name, 'bar');
+  assert.equal(ast.type, 'ObjectLiteral');
+  assert.equal(ast.properties[0].name, 'foo');
+  assert.equal(ast.properties[1].spread.type, 'Identifier');
+  assert.equal(ast.properties[1].spread.name, 'bar');
 });
 
 test('array literals', t => {
   const input = '[[,,,], [,5,,], [...a,, 5, ...b]]';
   const ast = parser(input);
 
-  t.snapshot(ast);
+  snapshot(ast, t.name);
 });
 
 test('destructuring', t => {
   const input = '([{y: {z1 = 5, ...z2} = {x:6}}, z3, ...z4]) => (z1 * z2.x) * z3 * z4.length';
   const ast = parser(input);
 
-  t.snapshot(ast);
+  snapshot(ast, t.name);
 });
 
-test('pos and text for bound short-hand parameters', t => {
+test('pos and text for bound short-hand parameters', () => {
   const input = '({ x, y = 5 }) => x * y'
   const ast = parser(input);
 
-  t.is(ast.type, 'ArrowFunction');
+  assert.equal(ast.type, 'ArrowFunction');
 
-  t.is(ast.parameters.bound[0].pattern.bindingType, 'ObjectPattern');
-  t.is(ast.parameters.bound[0].pattern.bound[0].prop, 'x');
-  t.is(ast.parameters.bound[0].pattern.bound[0].pattern.bindingType, 'SingleName');
-  t.is(ast.parameters.bound[0].pattern.bound[0].pattern.name, 'x');
-  t.is(ast.parameters.bound[0].pattern.bound[0].pattern.text, 'x');
-  t.is(ast.parameters.bound[0].pattern.bound[0].pattern.pos, 3);
-  t.is(ast.parameters.bound[0].pattern.bound[1].pattern.text, 'y');
+  assert.equal(ast.parameters.bound[0].pattern.bindingType, 'ObjectPattern');
+  assert.equal(ast.parameters.bound[0].pattern.bound[0].prop, 'x');
+  assert.equal(ast.parameters.bound[0].pattern.bound[0].pattern.bindingType, 'SingleName');
+  assert.equal(ast.parameters.bound[0].pattern.bound[0].pattern.name, 'x');
+  assert.equal(ast.parameters.bound[0].pattern.bound[0].pattern.text, 'x');
+  assert.equal(ast.parameters.bound[0].pattern.bound[0].pattern.pos, 3);
+  assert.equal(ast.parameters.bound[0].pattern.bound[1].pattern.text, 'y');
 });
